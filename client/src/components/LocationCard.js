38bac26d2c1c846523828c4d0455a533c08f692f
zuri227 <zuri22780@gmail.com>
2025-02-05 15:06:59 +0100

    updated card component and fetched location data

--- client/src/components/LocationCard.js
+++ client/src/components/LocationCard.js
@@ -1,11 +1,27 @@
 import React from 'react'
 import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
+import Typography from '@mui/material/Typography'
 
-const LocationCard = () => {
+const LocationCard = (location) => {
+
   return (
-    <div>
-      <Card >
-        
+    <div style={{marginRight: '15px'}}>
+      <Card sx={{ width: 275, border: 'solid 1pt'}}>
+        <CardContent>
+          <Typography variant='h5'>
+           {location.location.name}
+          </Typography>
+          <Typography>
+            Country: {location.location.address.country}
+          </Typography>
+          <Typography>
+            City: {location.location.address.city}
+          </Typography>
+          <Typography>
+            Address: {location.location.address.streetAddress}
+          </Typography>
+        </CardContent>
       </Card>
     </div>
   )
--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PageLayout from '../components/PageLayout';
+import LocationCard from '../components/LocationCard';
+
 
 const HomePage = () => {
+  const [locations, setLocations] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:3001/api/locations')
+    .then(response => response.json())
+    .then(data => setLocations(data))
+    .catch(error => console.error('Error Fetching Locations:', error))
+  }, [])
+
+
   return (
-    <div >
+    <div style={{margin: '10px'}}>
       <PageLayout />
       <h1>All Locations</h1>
+      <div style={{display: 'flex'}}>
+        {locations.map(location => (
+          <LocationCard key={location._id} location={location}/>
+        ))}
+      </div>
     </div>
   );
 };
